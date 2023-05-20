5f9ec76b88c0543293bb6182e0d542d0f29900a4
Moshik Hershcovitch <moshik1@gmail.com>
2023-05-20 17:10:34 +0300

    mereg diffrences from computers

--- plot_side_surf/base_real_get_request_per_hacked_key.py
+++ plot_side_surf/base_real_get_request_per_hacked_key.py
@@ -28,42 +28,54 @@ def start_point(array):
 
 
 
-def plot_(data_path1, data_path2):
+def plot_base(data_path1, data_path2):
     with open(data_path1, 'r') as f:
         reader = csv.reader(f, delimiter=',')
         headers = next(reader)
-        get_request = []
-        _keys = []
+        get_request_base = []
+        full_base_keys = []
         for row in reader:
-            get_request.append(row[0])
-            _keys.append(row[1])
+            get_request_base.append(row[0])
+            full_base_keys.append(row[1])
 
     with open(data_path2, 'r') as f:
         reader = csv.reader(f, delimiter=',')
         headers = next(reader)
         get_request_real = []
-        _keys_real = []
+        full_keys_real = []
         for row in reader:
             get_request_real.append(row[0])
-            _keys_real.append(row[1])
+            full_keys_real.append(row[1])
 
 
-    get_request = np.array(get_request, dtype=float)
-    _keys = np.array(base_keys, dtype=float)
+
+    get_request_base = np.array(get_request_base, dtype=float)
+    full_base_keys = np.array(full_base_keys, dtype=float)
+
+    base_keys = get_request_base / full_base_keys     
+
+
 
     get_request_real = np.array(get_request_real, dtype=float)
-    _keys_real = np.array(base_keys_real, dtype=float)
+    full_keys_real = np.array(full_keys_real, dtype=float)
 
-    _color = "#d95f0e"
+    keys_real = get_request_real / full_keys_real     
+
+
+
+
+
+    base_color = "#d95f0e"
     real_color = "#1b9e77"
     
-    plt.plot(get_request.astype(float), _keys.astype(float), label='SuRF_base', color= base_color)
-    plt.plot(get_request_real.astype(float), _keys_real.astype(float), label='SuRF_real', color= real_color)
+    plt.plot(get_request_base.astype(float), base_keys.astype(float), label='SuRF-Base', color= base_color)
+    plt.plot(get_request_real.astype(float), keys_real.astype(float), label='SuRF-Real', color= real_color)
     
     plt.grid()
-    plt.ylim([0,120])
-    plt.ylabel('Avg. get requests per hacked key (Millions)', fontsize=12)
-    plt.xlabel('Get Request (Millions)', fontsize=12)
+    plt.ylim([0,100])
+    plt.xlim([0,4000])
+    plt.ylabel('Avg. get requests per extracted key (Millions)', fontsize=12)
+    plt.xlabel('Get Requests (Millions)', fontsize=12)
     plt.legend()
  
 
@@ -72,6 +84,6 @@ def plot_(data_path1, data_path2):
 
 
 plt.figure(1)
-plot_base("data_base.csv", "data_base_real.csv")
-plt.savefig('fig/_average_gets_per_hacked_key.pdf')
+plot_base("data_response_time/50M_base_25-40_DB_1_10000000_guesses.txt", "data_response_time/50M_real_25-40_DB_1_10000000_guesses.txt")
+plt.savefig('fig/base_real_average_gets_per_hacked_key.pdf')
 plt.show()
